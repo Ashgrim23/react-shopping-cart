ca40127dd1afaddf2599ecf3718afab6c757e3a3
Ruben Francisco Solis Uzarraga <ruben.solisu@fovissste.gob.mx>
2020-10-30 19:43:02 -0600

    add redux products

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,13 +3,18 @@ import formatCurrency from '../util'
 import Fade from "react-reveal/Fade"
 import Modal from "react-modal"
 import Zoom from "react-reveal/Zoom"
+import { connect } from 'react-redux'
+import {fetchProducts} from "../actions/productActions"
 
-export default class Products extends Component {
+class Products extends Component {
     constructor(props){
         super(props)
         this.state={
             product:null
         }
+    }
+    componentDidMount(){
+        this.props.fetchProducts()
     }
     openModal=(product)=>{
         this.setState({product:product})
@@ -22,7 +27,8 @@ export default class Products extends Component {
         return (
             <div>
                 <Fade bottom cascade={true}>
-                <ul className="products">                    
+                {!this.props.products ? <div>Loading..</div> :
+                    <ul className="products">                    
                         {this.props.products.map((product)=>(
                             <li key={product._id}>
                                 <div className="product">
@@ -36,9 +42,9 @@ export default class Products extends Component {
                                     </div>
                                 </div>
                             </li>
-                        ))}
-                    
-                </ul>     
+                        ))}            
+                    </ul>     
+                }                
                 </Fade>           
                 {product && 
                     <Modal isOpen={true} onRequestClose={this.closeModal}>                        
@@ -70,3 +76,5 @@ export default class Products extends Component {
         )
     }
 }
+
+export default connect((state)=>({products:state.products.items}),{fetchProducts})(Products)
