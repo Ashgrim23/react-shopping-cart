7d34307065be8dc068ad887d4502a43bbbedaee4
Ruben Francisco Solis Uzarraga <ruben.solisu@fovissste.gob.mx>
2020-11-03 15:17:57 -0600

    add checkout modal

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import {createStore,applyMiddleware,combineReducers,compose} from 'redux'
 import thunk from 'redux-thunk'
 import { cartReducer } from './reducers/cartReducers';
+import { orderReducer } from './reducers/orderReduciers';
 import {productsReducer } from './reducers/productReducers'
 
 const initialState={};
 const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store =createStore(combineReducers({
     products: productsReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    order:orderReducer
 }),
 initialState,
 composeEnhancer(applyMiddleware(thunk))
